a0b9230b612da9a3c35a4376cf438305fe11cbfd
RudraRaval7840 <158600454+RudraRaval7840@users.noreply.github.com>
2024-04-01 10:45:37 +0530

    added files

---
+++ src/screen/Navigation/AppNavigater.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AppNavigater = () => {
+  return (
+    <View>
+      <Text>AppNavigater</Text>
+    </View>
+  )
+}
+
+export default AppNavigater
